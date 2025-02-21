ca9266cecdf12103bde13bb57c9efcfb6a43632b
mugaabus <fatakimugaruka91@gmail.com>
2025-02-21 08:56:02 +0300

    allow the users to navigate throught categories

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,20 @@ class CarSellingApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String selectedCategory = 'All';
+
+  void updateCategory(String category) {
+    setState(() {
+      selectedCategory = category;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,13 +47,12 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
           ),
+          CategorySection(
+            selectedCategory: selectedCategory,
+            onCategorySelected: updateCategory,
+          ),
           Expanded(
-            child: ListView(
-              children: [
-                CategorySection(),
-                CarList(),
-              ],
-            ),
+            child: CarList(selectedCategory: selectedCategory),
           ),
         ],
       ),
@@ -50,6 +62,10 @@ class HomeScreen extends StatelessWidget {
 
 class CategorySection extends StatelessWidget {
   final List<String> categories = ['All', 'New', 'Used', 'SUV', 'Sedan', 'Truck'];
+  final String selectedCategory;
+  final Function(String) onCategorySelected;
+
+  CategorySection({required this.selectedCategory, required this.onCategorySelected});
 
   @override
   Widget build(BuildContext context) {
@@ -59,11 +75,15 @@ class CategorySection extends StatelessWidget {
         scrollDirection: Axis.horizontal,
         itemCount: categories.length,
         itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Chip(
-              label: Text(categories[index]),
-              backgroundColor: Colors.grey.shade200,
+          return GestureDetector(
+            onTap: () => onCategorySelected(categories[index]),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Chip(
+                label: Text(categories[index]),
+                backgroundColor: categories[index] == selectedCategory ? Colors.blueAccent : Colors.grey.shade200,
+                labelStyle: TextStyle(color: categories[index] == selectedCategory ? Colors.white : Colors.black),
+              ),
             ),
           );
         },
@@ -73,16 +93,27 @@ class CategorySection extends StatelessWidget {
 }
 
 class CarList extends StatelessWidget {
+  final String selectedCategory;
   final List<Map<String, String>> cars = [
-    {'name': 'Tesla Model S', 'price': '\$50,000', 'image': 'assets/noa.jpeg'},
-    {'name': 'BMW X5', 'price': '\$40,000', 'image': 'assets/tx.jpeg'},
-    {'name': 'Toyota Camry', 'price': '\$25,000', 'image': 'assets/vanette.jpeg'},
+    {'name': 'Tesla Model S', 'price': '\$50,000', 'image': 'assets/noa.jpeg', 'category': 'New'},
+    {'name': 'BMW X5', 'price': '\$40,000', 'image': 'assets/tx.jpeg', 'category': 'SUV'},
+    {'name': 'Toyota Camry', 'price': '\$25,000', 'image': 'assets/vaguard.jpeg', 'category': 'Sedan'},
+    {'name': 'Ford F-150', 'price': '\$35,000', 'image': 'assets/vanette.jpeg', 'category': 'Truck'},
+    {'name': 'Honda Civic', 'price': '\$20,000', 'image': 'assets/noa.jpeg', 'category': 'Used'},
   ];
+
+  CarList({required this.selectedCategory});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: cars.map((car) {
+    List<Map<String, String>> filteredCars = selectedCategory == 'All'
+        ? cars
+        : cars.where((car) => car['category'] == selectedCategory).toList();
+
+    return ListView.builder(
+      itemCount: filteredCars.length,
+      itemBuilder: (context, index) {
+        final car = filteredCars[index];
         return Card(
           margin: EdgeInsets.all(10),
           child: ListTile(
@@ -93,7 +124,7 @@ class CarList extends StatelessWidget {
             onTap: () {},
           ),
         );
-      }).toList(),
+      },
     );
   }
 }
